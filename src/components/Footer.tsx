68c21a7e9bd548589005e8a11616f0327605c672
Vivek Singh <viveksingh9301@gmail.com>
2026-06-16 22:32:27 +0530

    Add Footer component with branding-like information

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="mt-24 p-16 bg-black text-white">
+      <h5 className="text-lg">Interactive scroll-driven motion design experience.</h5>
+      <h1 className="text-7xl font-bold">LENIS & GSAP.</h1>
+    </footer>
+  );
+}
