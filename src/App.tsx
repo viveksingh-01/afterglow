2c8d55986752acdf5370d78a71ba60b432bc1b3b
Vivek Singh <viveksingh9301@gmail.com>
2026-06-18 20:07:44 +0530

    Add GSAP ticker integration in App component for smooth animation updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import gsap from "gsap";
 import ReactLenis, { type LenisRef } from "lenis/react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Gallery from "./components/Gallery";
 
 function App() {
   const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
 
   return (
     <>
