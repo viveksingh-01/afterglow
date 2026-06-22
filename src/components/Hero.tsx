451efeaf10858f74652bb941d69cf55551776530
Vivek Singh <viveksingh9301@gmail.com>
2026-06-22 21:24:50 +0530

    Update Hero component title spacing for improved visual impact

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <section className="w-full h-screen fixed top-0 left-0 flex justify-center items-center">
-      <h1 className="text-5xl text-amber-300 font-extralight tracking-widest">AFTERGLOW</h1>
+      <h1 className="text-5xl text-amber-300 font-extralight tracking-[90px]">AFTERGLOW</h1>
     </section>
   );
 };
